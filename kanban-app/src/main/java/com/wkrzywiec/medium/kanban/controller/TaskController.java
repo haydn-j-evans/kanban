a6adeed2dfc15753c30e8bfc271a1ed61e04169a
Haydn Evans <haydn_j_evans@hotmail.com>
2021-04-30 09:14:02 +0200

    Update TaskController.java

--- kanban-app/src/main/java/com/wkrzywiec/medium/kanban/controller/TaskController.java
+++ kanban-app/src/main/java/com/wkrzywiec/medium/kanban/controller/TaskController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/tasks")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class TaskController {
 
     private final TaskService taskService;
